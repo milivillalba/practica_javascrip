5888048f37872d6ce51a87aa7dc2fa0cdfbb68b7
milivillalba <soledad110203@gmail.com>
2023-05-17 16:44:01 -0300

    practica javascrip

---
+++ app.js
@@ -0,0 +1,15 @@
+//objetos literales
+//let ,const /los objetos  como constantes se pueden malipular
+
+let notebook = {
+  //propiedad con su valor
+  marca: "Lenovo",
+  precio: 195000,
+}; // un objeto se coloca  dentro de llaves
+
+let notebookHp = {
+  marca: "Hp",
+  precio: 300000,
+};
+
+console.log(notebook);
